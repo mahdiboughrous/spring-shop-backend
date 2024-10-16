7dada04852ea6516f1592b832b2346c4139dc6dc
mahdibough6 <mahdibough6@gmail.com>
2024-10-16 21:16:47 +0100

    add openfeign link between product-composite microservce and others ( product-service, review-service, recommendation-service )

---
+++ product-service/src/main/java/com/boughrous/product_service/dto/CreateProductRequest.java
@@ -0,0 +1,18 @@
+package com.boughrous.product_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CreateProductRequest {
+
+    private String name;
+    private Double weight;
+
+}
